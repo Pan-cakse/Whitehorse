883d82198ea16b25698e7d068a330c5ca89be320
VidTu <60929459+VidTu@users.noreply.github.com>
2022-06-22 00:58:35 +0300

    Release

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://papermc.io/repo/repository/maven-public/")
+    }
+}
+
+rootProject.name = "whitehorse"
+include("whitehorse-api", "whitehorse-server")
