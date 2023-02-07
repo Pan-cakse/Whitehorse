7e119ed0cb147a3136db78339455e70db67df678
VidTu <60929459+VidTu@users.noreply.github.com>
2023-02-07 06:31:53 +0300

    1.19.3 + Purpur

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,19 +3,19 @@ import io.papermc.paperweight.util.constants.PAPERCLIP_CONFIG
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-    id("io.papermc.paperweight.patcher") version "1.3.8"
+    id("io.papermc.paperweight.patcher") version "1.4.1"
 }
 
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/") {
+    maven("https://repo.papermc.io/repository/maven-public/") {
         content { onlyForConfigurations(PAPERCLIP_CONFIG) }
     }
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.2:fat")
-    decompiler("net.minecraftforge:forgeflower:1.5.605.7")
+    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
+    decompiler("net.minecraftforge:forgeflower:2.0.605.1")
     paperclip("io.papermc:paperclip:3.0.2")
 }
 
@@ -25,7 +25,7 @@ subprojects {
         toolchain { languageVersion.set(JavaLanguageVersion.of(17)) }
     }
     tasks.withType<JavaCompile>().configureEach {
-        options.encoding = "UTF-8"
+        options.encoding = Charsets.UTF_8.name()
         options.release.set(17)
     }
     tasks.withType<Javadoc> {
@@ -35,14 +35,8 @@ subprojects {
         filteringCharset = Charsets.UTF_8.name()
     }
     repositories {
-        mavenLocal()
         mavenCentral()
-        maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://papermc.io/repo/repository/maven-public/")
-        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
-        maven("https://repo.aikar.co/content/groups/aikar")
-        maven("https://repo.md-5.net/content/repositories/releases/")
-        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven("https://repo.papermc.io/repository/maven-public/")
         maven("https://jitpack.io")
     }
 }
@@ -51,8 +45,12 @@ paperweight {
     serverProject.set(project(":whitehorse-server"))
     remapRepo.set("https://maven.fabricmc.net/")
     decompileRepo.set("https://files.minecraftforge.net/maven/")
-    usePaperUpstream(providers.gradleProperty("paper")) {
-        withPaperPatcher {
+    useStandardUpstream("purpur") {
+        url.set(github("PurpurMC", "Purpur"))
+        ref.set(providers.gradleProperty("purpur"))
+        withStandardPatcher {
+            apiSourceDirPath.set("Purpur-API")
+            serverSourceDirPath.set("Purpur-Server")
             apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
             serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
             apiOutputDir.set(layout.projectDirectory.dir("whitehorse-api"))
